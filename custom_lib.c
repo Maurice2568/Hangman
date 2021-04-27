c40227b833c2b5046253222228d1da9e4873aff0
LinKaKling <kasimirklinger@gmx.de>
2021-04-27 15:25:34 +0200

    c

--- custom_lib.c
+++ custom_lib.c
@@ -17,14 +17,22 @@ int Menu()
     fflush(stdin);
     scanf(" %c",&input[0]);
 
-    while (stop == 0){
-        if (input[0] == '1'){
+    while (stop == 0)
+    {
+        if (input[0] == '1')
+        {
             Play();
-        }else if (input[0] == '2'){
+        }
+        else if (input[0] == '2')
+        {
             Play();
-        }else if (input[0] == '3'){
+        }
+        else if (input[0] == '3')
+        {
             WriteFile();
-        }else if (input[0] == '4'){
+        }
+        else if (input[0] == '4')
+        {
             printf("Auf Wiedersehen\n\n\n\n");
             stop = 1;
         }
@@ -39,38 +47,68 @@ int Menu()
 12 Menu
 13 newlines to fit in the Window
 */
-int Screen(int page){
+int Screen(int page)
+{
     char player1[];
-    if (page == 0){
-            printf("Folgende Buchstaben wurden schon geraten: ");//empty
-    }else if(page == 1){
-            printf("00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n\n\n");
-    }else if(page == 2){//Hangmans following
-            printf("00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 3){
-            printf("0000000000000000000000000000000000000000\n00\n00\n00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 4){
-            printf( "0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 5){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 6){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 7){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00                             333   \n00   00                            333   \n00    00                           333   \n00     00                          333   \n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 8){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 9){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                3333333333\n00                               3 3333<3333 \n000                             3  333333333\n0000                           3   333333333\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 10){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                33333333333\n00                               3 3333<3333 3 \n000                             3  333333333  3\n0000                           3   333333333   3\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 11){
-            printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |x  x |\n00                                  |___  |\n00                                    22\n00                                33333333333\n00                               3 3333<3333 3 \n000                             3  333333333  3\n0000                           3   333333333   3\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
-    }else if(page == 12){//startpage
-            printf("_______________________________________________________________________\n  Hallo und herzlich willkommen zu einer neuen Runde Hangman \n\n  Was moechtest du tun?\n 1 -Einzelspieler Partie starten\n 2 -Zwei Spieler Partie starten\n 3 -neue Loesungswoerter eintragen\n 4 -Programm beenden\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    }else if(page == 13){
-            printf("\n\n\n\n\n");
-    }else {
-            printf("nothing to print? %d",page);
+    if (page == 0)
+    {
+        printf("Folgende Buchstaben wurden schon geraten: ");//empty
+    }
+    else if(page == 1)
+    {
+        printf("00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n\n\n");
+    }
+    else if(page == 2)  //Hangmans following
+    {
+        printf("00\n00\n00\n00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 3)
+    {
+        printf("0000000000000000000000000000000000000000\n00\n00\n00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 4)
+    {
+        printf( "0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00\n00\n00\n00\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 5)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00\n00\n000\n0000\n00 00\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 6)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00\n00   00\n00    00\n00     00\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 7)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00                             333   \n00   00                            333   \n00    00                           333   \n00     00                          333   \n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 8)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                 333333333\n00                                 3333<3333 \n000                                333333333\n0000                               333333333\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 9)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                3333333333\n00                               3 3333<3333 \n000                             3  333333333\n0000                           3   333333333\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 10)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |o  o |\n00                                  |___  |\n00                                    22\n00                                33333333333\n00                               3 3333<3333 3 \n000                             3  333333333  3\n0000                           3   333333333   3\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 11)
+    {
+        printf("0000000000000000000000000000000000000000\n00                                    00\n00                                    00\n00                                    00\n00                                  !!!!!!\n00                                  |x  x |\n00                                  |___  |\n00                                    22\n00                                33333333333\n00                               3 3333<3333 3 \n000                             3  333333333  3\n0000                           3   333333333   3\n00 00                              333333333\n00  00                             333   333\n00   00                            333   333\n00    00                           333   333\n00     00                          333   333\n00      00\n00       00\n00        00\n\n\n");
+    }
+    else if(page == 12)  //startpage
+    {
+        printf("_______________________________________________________________________\n  Hallo und herzlich willkommen zu einer neuen Runde Hangman \n\n  Was moechtest du tun?\n 1 -Einzelspieler Partie starten\n 2 -Zwei Spieler Partie starten\n 3 -neue Loesungswoerter eintragen\n 4 -Programm beenden\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+    else if(page == 13)
+    {
+        printf("\n\n\n\n\n");
+    }
+    else
+    {
+        printf("nothing to print? %d",page);
     }
     return 0;
 }
@@ -162,82 +200,83 @@ int ReadFile()
 
     char input[64];
 
-    while(fgets(input, 63, fp)) {
+    while(fgets(input, 63, fp))
+    {
         for(i = 0; (i < 100 && input[i] != '\0'); i++)
-        input[i] = input[i] - 13;
-		sscanf(input, "%s", guessWords[WordsReadIn]);
-		WordsReadIn++;
-	}
+            input[i] = input[i] - 13;
+        sscanf(input, "%s", guessWords[WordsReadIn]);
+        WordsReadIn++;
+    }
 
     printf("Eingelesene Woerter: %d\n",WordsReadIn);
-fseek(fp, 0, SEEK_END); // goto end of file
-if (ftell(fp) == 0)
- {
-     printf("Leere Datei. \n"); //file empty
- }
+    fseek(fp, 0, SEEK_END); // goto end of file
+    if (ftell(fp) == 0)
+    {
+        printf("Leere Datei. \n"); //file empty
+    }
     fclose(fp); // close the file
     return 0;
 }
 
 int AppendFile()
 {
-int i;
-char input[255];
-char answer;
-printf("Loesungswort:");
-FILE *fp;
-fp  = fopen ("guesswords.dat", "a");
-fflush(stdin);
-fgets(input, 16, stdin);
-for(i = 0; (i < 100 && input[i] != '\0'); i++)
+    int i;
+    char input[255];
+    char answer;
+    printf("Loesungswort:");
+    FILE *fp;
+    fp  = fopen ("guesswords.dat", "a");
+    fflush(stdin);
+    fgets(input, 16, stdin);
+    for(i = 0; (i < 100 && input[i] != '\0'); i++)
         input[i] = input[i] + 13;
-fprintf(fp,input,"\n");
-printf("Weitere Woerter hinzufuegen? y/n\n");
-scanf(" %c",&answer);
-while(answer == 'y' || answer == 'j')
-{
-  printf("Loesungswort: ");
-  fflush(stdin);
-  fgets(input, 16, stdin);
-  for(i = 0; (i < 100 && input[i] != '\0'); i++)
-        input[i] = input[i] + 13;
-  fprintf(fp,input,"\n");
-  printf("Weitere Wörter hinzufuegen? y/n\n");
-  scanf(" %c",&answer);
-  fflush(stdin);
-}
-fclose(fp);
-return 0;
+    fprintf(fp,input,"\n");
+    printf("Weitere Woerter hinzufuegen? y/n\n");
+    scanf(" %c",&answer);
+    while(answer == 'y' || answer == 'j')
+    {
+        printf("Loesungswort: ");
+        fflush(stdin);
+        fgets(input, 16, stdin);
+        for(i = 0; (i < 100 && input[i] != '\0'); i++)
+            input[i] = input[i] + 13;
+        fprintf(fp,input,"\n");
+        printf("Weitere Wörter hinzufuegen? y/n\n");
+        scanf(" %c",&answer);
+        fflush(stdin);
+    }
+    fclose(fp);
+    return 0;
 }
 
 int WriteFile()
 {
-int i;
-char input[255];
-char answer;
-printf("Loesungswort:");
-FILE *fp;
-fp  = fopen ("guesswords.dat", "w");
-fflush(stdin);
-fgets(input, 16, stdin);
-for(i = 0; (i < 100 && input[i] != '\0'); i++)
+    int i;
+    char input[255];
+    char answer;
+    printf("Loesungswort:");
+    FILE *fp;
+    fp  = fopen ("guesswords.dat", "w");
+    fflush(stdin);
+    fgets(input, 16, stdin);
+    for(i = 0; (i < 100 && input[i] != '\0'); i++)
         input[i] = input[i] + 13;
-fprintf(fp,input);
-printf("Weitere Woerter hinzufuegen? y/n\n");
-scanf(" %c",&answer);
-while(answer == 'y' || answer == 'j')
-{
-  printf("Loesungswort: ");
-  fflush(stdin);
-  fgets(input, 16, stdin);
-  for(i = 0; (i < 100 && input[i] != '\0'); i++)
-        input[i] = input[i] + 13;
-  fprintf(fp,input);
-  printf("Weitere Woerter hinzufuegen? y/n\n");
-  scanf(" %c",&answer);
-  fflush(stdin);
-}
-fclose(fp);
-return 0;
+    fprintf(fp,input);
+    printf("Weitere Woerter hinzufuegen? y/n\n");
+    scanf(" %c",&answer);
+    while(answer == 'y' || answer == 'j')
+    {
+        printf("Loesungswort: ");
+        fflush(stdin);
+        fgets(input, 16, stdin);
+        for(i = 0; (i < 100 && input[i] != '\0'); i++)
+            input[i] = input[i] + 13;
+        fprintf(fp,input);
+        printf("Weitere Woerter hinzufuegen? y/n\n");
+        scanf(" %c",&answer);
+        fflush(stdin);
+    }
+    fclose(fp);
+    return 0;
 }
 
